f3784cf0fd50756014cf9ce2d6b989a6b6071c02
Trac Nguyen <nguyendtrac@gmail.com>
2018-02-17 20:45:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,14 +36,8 @@ def monopoly_with_third_tier
       },
       :names=>
       {
-        :reading_railroad=>
-        {
-          "mortgage_value"=>"$100"
-        },
-        :pennsylvania_railroad=>
-        {
-          "mortgage_value"=>"$200"
-        },
+        :reading_railroad=>{},
+        :pennsylvania_railroad=>{},
         :b_and_o_railroad=>{},
         :shortline=>{}
       }
